622fd2792c30e18edd89b04ed60b7c571159258a
Sana Elheni <elhenisana40@gmail.com>
2025-10-01 12:50:50 +0100

    code/add

--- Gestion_UE_Etudiant/src/main/java/webservices/UERessources.java
+++ Gestion_UE_Etudiant/src/main/java/webservices/UERessources.java
@@ -1,13 +1,15 @@
 package webservices;
 
+import entities.UniteEnseignement;
 import metiers.UniteEnseignementBusiness;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+@Path("/ue")
 public class UERessources {
     //instance ==> object
     UniteEnseignementBusiness helper = new UniteEnseignementBusiness();
@@ -21,5 +23,26 @@ public class UERessources {
                 .build();
     }
 
+    @Path("/code/{code}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
 
+    public Response getByCode(@PathParam("code")int code){
+        return Response
+                .status(200)
+                .entity(this.helper.getUEByCode(code))
+                .build();
+    }
+
+
+    @Path("/add")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public Response add( UniteEnseignement ue){
+        return Response
+                .status(200)
+                .entity(this.helper.addUniteEnseignement(ue))
+                .build();
+    }
 }
